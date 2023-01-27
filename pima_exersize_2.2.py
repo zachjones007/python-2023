877f012781814fc952708dcf9b0f7e1445baef22
Zachary Jones <86993107+zachjones007@users.noreply.github.com>
2023-01-27 07:18:37 -0700

    Create pima_exersize_2.2.py

---
+++ pima_exersize_2.2.py
@@ -0,0 +1,5 @@
+target_heart_rate_low = max_heart_rate * 0.5
+target_heart_rate_high = max_heart_rate * 0.85
+
+print("Your maximum heart rate is: " + str(max_heart_rate) + " beats per minute")
+print("Your target heart rate range is between: " + str(target_heart_rate_low) + " and " + str(target_heart_rate_high) + " beats per minute")
