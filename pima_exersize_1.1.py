744cdf1b711f064a5c48ade7351c613923c2938a
Zachary Jones <86993107+zachjones007@users.noreply.github.com>
2023-01-27 07:14:13 -0700

    Update pima_exersize_1.1.py

--- pima_exersize_1.1.py
+++ pima_exersize_1.1.py
@@ -1,9 +1,7 @@
-p=1000
-r=0.07
-a=10
-b=20
-c=30
+def numbers (p,r,a,b,c):
 
-for n in [a,b,c]:
-    a = p * (1 + r)**n
-    print(f"year : {n} return : ${a}")
+
+    for n in [a,b,c]:
+        a = p * (1 + r)**n
+        print(f"year : {n} return : ${a}")
+numbers (1000,0.07,10,20,30)
