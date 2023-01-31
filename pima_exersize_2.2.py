5d4668bc5bc6c8447719a5b310f6be2a31053a7e
Zachary Jones <86993107+zachjones007@users.noreply.github.com>
2023-01-31 07:51:28 -0700

    Update pima_exersize_2.2.py

--- pima_exersize_2.2.py
+++ pima_exersize_2.2.py
@@ -1,7 +1,8 @@
-age = int(input("Please enter your age: "))
-max_heart_rate = 220 - age
-target_heart_rate_low = max_heart_rate * 0.5
-target_heart_rate_high = max_heart_rate * 0.85
+def numbers (twotwenty,pointfive,pointeight):
+    age = int(input("Please enter your age: "))
+    max = twotwenty - age
+    low = max * pointfive
+    high = max * pointeight
 
-print("Your maximum heart rate is: {:.2f} beats per minute".format(max_heart_rate))
-print("Your target heart rate range is between: {:.2f} and {:.2f} beats per minute".format(target_heart_rate_low, target_heart_rate_high))
+    print(f"low : {low:.1f} high : {high:.1f}")
+numbers(220,0.5,0.85)
