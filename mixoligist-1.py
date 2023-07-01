5fc6d7fc705fe20c20ab23d2fafa7ecb772e47a6
Zachary Jones <86993107+zachjones007@users.noreply.github.com>
2023-07-01 04:26:40 -0700

    Add files via upload

---
+++ mixoligist-1.py
@@ -0,0 +1,18 @@
+x = 7/3
+y = 2/3
+print (x+y)
+print(x-y)
+print(x/y)
+def function(a,b):
+    print(f"{a}/{b}")
+    a1 =1 
+    b1 =1 
+    print(b1,a1)
+z1 = complex(2, 3)  # 2 + 3j
+y1 = complex(1,2)
+print(z1+y1)
+print(z1-y1)
+t = (2 + 1j)
+print(t)
+print(t.real)
+print(t.imag)
